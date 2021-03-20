2c33f8e9f55c1d6b11bf03d49941b030113c85c4
Nitish Kumar <nit5343@hotmail.com>
2021-03-20 22:32:33 +0530

    Add Auth0

--- routes.js
+++ routes.js
@@ -7,14 +7,6 @@ const router = express.Router();
 
 const configurations = [
     {
-        organisationCode: "org2",
-        idpOptions: {
-            sso_login_url: " https://justintegrated.com/",
-            sso_logout_url: " https://justintegrated.com/",
-            certificates: [fs.readFileSync('justintegrated-idp.crt.pem').toString()]
-        },
-    },
-    {
         organisationCode: "org1",
         idpOptions: {
             sso_login_url: "https://login.microsoftonline.com/7bb620d5-949e-433e-af27-f9c0f08034ee/saml2",
@@ -22,6 +14,14 @@ const configurations = [
             certificates: [fs.readFileSync('azure-idp.crt.pem').toString()]
         },
     },
+    {
+        organisationCode: "org2",
+        idpOptions: {
+            sso_login_url: "https://dev-umang-f1.auth0.com/samlp/FsORMWQXTYu42C2YgLRzrTtqwfY5LNiB",
+            sso_logout_url: "https://dev-umang-f1.auth0.com/samlp/FsORMWQXTYu42C2YgLRzrTtqwfY5LNiB/logout",
+            certificates: [`-----BEGIN CERTIFICATE-----MIIC8DCCAdigAwIBAgIQFhFdD0IFxLZMwlBxyN0drjANBgkqhkiG9w0BAQsFADA0MTIwMAYDVQQDEylNaWNyb3NvZnQgQXp1cmUgRmVkZXJhdGVkIFNTTyBDZXJ0aWZpY2F0ZTAeFw0yMDA4MTAwNjMzMDBaFw0yMzA4MTAwNjMzMDBaMDQxMjAwBgNVBAMTKU1pY3Jvc29mdCBBenVyZSBGZWRlcmF0ZWQgU1NPIENlcnRpZmljYXRlMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuBHFN6n8mdSSBNrzsLKtSbEnTYZ93tGG5TUnoNStqLxjJJxJPqqchp37X9IQ0uHdRlhKOKX5WZaRe7+zIGAhEsH16nC0dwMbJHkdxbisZDUEIo3OFdtPJqx80AMvlIw0HTtNplYAa/yMQ67FpjqLLv8GOTdNgufDrytohA98jTA+fDE0hxrKj2xmyDG7tiZ4aiPIVjRFA9+fe/azNcoY+N6piQkoSYkH8mFPnjdsqYD6FccPtKyOHSJYkHBDbr3sq+EkbdHPfWpDjJngPrhno3SvnXYTbCbeSUVaZYFUvVY9RYFzhUYDeBn5hwrZbfcvoMM9Z4fYgJE9AkKLKjP4qQIDAQABMA0GCSqGSIb3DQEBCwUAA4IBAQBeFLxg5OUtfY0mvKjPtNVdYatCqmJUyRzNaEaGGxUKjKhC6jSVzTZyIeC12D4DB3Fri1lP1cQ1xgTb1MsXskxmpS145Kwxw/0wUBuuFYMtKz+MASyicO26BHMNZe1nCNcVo7ohPZxHLy/u1zMMjkpjd+RzbbAKqyLi7l1pE6n4UUnXxPSQICk/yYwqp6CCfsZobKGm3s9k0M5ATzrlfPgfnt/JUI/YgdqY6L9zhsXHrFQ13/DJYpNwvEtHjlPFD5Qieueyc1yWOF2L5VTDK7PIEtzS1koi4uKMhEnQt2rqLt32K/pjZ9IpTKvlZxxtAiQ+rg760C34Ho2mN1IcQ+IA-----END CERTIFICATE-----`]
+        },
+    }
 ]
 
 function getConfigurations(ms) {
@@ -73,7 +73,7 @@ const init = async () => {
                 if (err != null) {
                     return res.send(500);
                 }
-                console.log({ saml_response });
+                console.log(JSON.stringify({ saml_response }, null, 2));
                 // Sign JWT token & return
                 const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000"
                 res.redirect(`${CLIENT_URL}?token=token`);
